9df33fb0859172d52981cced5cbea41a495901ff
Mark Lawrence Peralta <vfranco123456@gmail.com>
2023-02-17 21:28:47 +0800

    a

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -11,7 +11,7 @@
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
     </head>
 
 <body onload="slider()">
@@ -41,7 +41,7 @@
                                     <a class="nav-link" aria-current="page" href="{{ url('/carmodel')}}">Vehicles</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ url('/termsandconditions')}}">Terms</a>
+                                    <a class="nav-link" href="{{ url('/terms')}}">Terms</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ url('/aboutus') }}">About Us</a>
