52f3c4f9769b561e4f674abcbc03917b79650abe
Mark Lawrence Peralta <vfranco123456@gmail.com>
2023-02-17 16:15:13 +0800

    a

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -38,7 +38,7 @@
                                     <a class="nav-link" aria-current="page" href="{{ url('/')}}">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="{{ url(' /carmodel')}}">Vehicles</a>
+                                    <a class="nav-link" aria-current="page" href="{{ url('/carmodel')}}">Vehicles</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ url('/termsandconditions')}}">Terms</a>
