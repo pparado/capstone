5898a89a54b8fd9964c7f63013e45230945c9443
Mark Lawrence Peralta <vfranco123456@gmail.com>
2023-02-17 14:44:04 +0800

    a

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-</head>
+    </head>
 
 <body onload="slider()">
      <div class="banner">
@@ -38,7 +38,7 @@
                                     <a class="nav-link" aria-current="page" href="{{ url('/')}}">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="{{ url('/carmodel')}}">Vehicles</a>
+                                    <a class="nav-link" aria-current="page" href="{{ url(' /carmodel')}}">Vehicles</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ url('/termsandconditions')}}">Terms</a>
